aa0789228ec3856ab6b9e161e82764476c8fd65d
CK151 <andersonoki15@gmail.com>
2022-10-30 15:14:55 +0300

    init

---
+++ script.js
@@ -0,0 +1,29 @@
+//variables
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+
+btn.addEventListener("click", palidrome);
+
+
+
+function palidrome() {
+    const word =document.querySelector(".input-text").value;
+
+    let len = word.length;
+    let start = word.substring(0,Math.floor(len/2)).toLowerCase();
+    //alert(start);
+    let end = word.substring(len-Math.floor(len/2)).toLowerCase();
+
+    //let flip = end.split("").reverse().join("")
+    let flip = [...end].reverse().join("");
+
+    if (start == flip) {
+        result.innerHTML = `${word.toUpperCase()} is a palidrome`;
+    }
+    else {
+        result.innerHTML = `${word.toUpperCase()} is NOT a palidrome`;
+    }
+    //alert(flip);
+}
